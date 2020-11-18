d93a077cd7e94f57d438e4ab24fac0a772c39cc5
Sophia Belkhir <sophia.belkhir@ens-lyon.fr>
2020-11-18 15:51:53 +0000

    Script quantification of expression with salmon index and salmon quant

---
+++ salmon_quantif.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#Script to create index with salmon for quantification
+#Create working directory
+data="/home/rstudio/data/mydatalocal/data_from_ftp"
+mkdir -p $data
+cd $data
+
+#Create directory for the current task (salmon index and quantif)
+output_index="outputs_salmon"
+mkdir -p $output_index
+cd $output_index
+
+#run salmon index
+salmon index -t $data/trinity_data/Trinity_RF.fasta -i $data/$output_index -p 4
+
+#index ok 
+
+#Library of each pair of reads, to use in the loop for salmon quant
+library_reads="Lib1_31_20_S1
+Lib2_31_20_S2
+Lib3_31_20_S3
+Lib4_31_20_S4
+Lib5_31_20_S5
+Lib6_31_20_S6"
+
+#run salmon quant for each sample
+for lib in $library_reads
+do
+  salmon quant -i $data/$output_index -l A -1 $data/outputs_trimmomatic/${lib}_1_paired.fq.gz -2 $data/outputs_trimmomatic/${lib}_2_paired.fq.gz --validateMappings -o $data/$output_index
+done
