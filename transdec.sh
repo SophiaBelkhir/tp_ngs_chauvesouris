5fb7032793ea9d3af7325ea3cceb179cfb960cfa
SophiaBelkhir <sophia.belkhir@ens-lyon.fr>
2020-12-19 13:33:52 +0000

    Modified comments

--- transdec.sh
+++ transdec.sh
@@ -12,8 +12,8 @@ mkdir -p $output_transdec
 
 #launch Transdecoder
 
-# Step 1: extract the long open reading frames
+#Step 1: extract the long open reading frames
 TransDecoder.LongOrfs -t $data/trinity_data/Trinity_RF.fasta --gene_trans_map $data/trinity_data/Trinity_RF.fasta.gene_trans_map -S -O $output_transdec
 
-#Step 2: predict the ORFs
+#Step 2: predict the coding regions
 TransDecoder.Predict -t $data/trinity_data/Trinity_RF.fasta --single_best_only -O $output_transdec
